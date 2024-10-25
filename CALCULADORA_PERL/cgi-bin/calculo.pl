1d5e4bf37f10b63303ade129d457d12395553ee7
ChipanaGerman <gchipanaj@unsa.edu.pe>
2024-10-24 19:10:18 -0500

    Actualizando archivo calculo.pl

--- CALCULADORA_PERL/cgi-bin/calculo.pl
+++ CALCULADORA_PERL/cgi-bin/calculo.pl
@@ -9,4 +9,25 @@ my $cgi = CGI->new;
 # Obtener la expresión del formulario
 my $expresion = $cgi->param('expresion') || '';
 
+# Función para calcular la expresión
+sub calcular_expresion {
+    my $expr = shift;
 
+    # Eliminar espacios en blanco
+    $expr =~ s/\s+//g;
+
+    # Verificar que la expresión contenga solo números, operadores y paréntesis
+    unless ($expr =~ /^[\d\+\-\*\/\(\)\.]+$/) {
+        return 'Error: Expresión inválida';
+    }
+
+    # Resolver los paréntesis primero usando recursión
+    while ($expr =~ /\(([^()]+)\)/) {
+        my $sub_expr = $1;
+        my $resultado_sub_expr = calcular_operacion($sub_expr);
+        $expr =~ s/\Q($sub_expr)\E/$resultado_sub_expr/;
+    }
+
+    # Resolver la expresión final sin paréntesis
+    return calcular_operacion($expr);
+}
